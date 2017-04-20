647154b6e7534cbf851cc3e2aa2b949b96b206a1
hellosylvee <helloimsylvia@gmail.com>
2017-04-20 16:39:52 -0400

    refactored user_vocab controller

--- app/controllers/user_vocabs_controller.rb
+++ app/controllers/user_vocabs_controller.rb
@@ -9,31 +9,44 @@ class UserVocabsController < ApplicationController
     word_strings = params[:user_words][:words]
     type_strings = params[:user_words][:word_types]
     @user = User.find( session[:user_id] )
+    @madlib = Madlib.find( params["madlib_id"] )
+    @user_vocab = UserVocab.create( user_id: @user.id, madlib_id: @madlib.id )
 
-    # @words = word_strings.map.with_index do |word, index|
-    #   type_instance = Type.find_by(word_type: type_strings[index])
-    #   @user.words.create( word: word, type: type_instance )
+    @user_words = word_strings.map.with_index do |word, index|
+      type_instance = Type.find_by(word_type: type_strings[index])
+      word = Word.find_or_create_by(word: word, type: type_instance)
+      UserWord.create(user: @user, user_vocab: @user_vocab, word: word)
+      # @user.words.find_or_create_by( word: word, type: type_instance )
+    end
+
+    redirect_to madlib_user_vocab_path(@madlib, @user_vocab)
+
+    # byebug
+    # words_hash = Hash[word_strings.zip(type_strings)]
+    # @words = words_hash.each_with_object([]) do |(word, type),array|
+    #   type_object = Type.find_by( word_type: type )
+    #   array << Word.find_or_create_by( word: word, type: type_object )
+    # end
+    #
+    # @user = User.find( session[:user_id] )
+    # @user_words = @words.map do |word|
+    #   UserWord.create( user_id: @user.id, word_id: word.id)
     # end
 
-    words_hash = Hash[word_strings.zip(type_strings)]
-    @words = words_hash.each_with_object([]) do |(word, type),array|
-      type_object = Type.find_by( word_type: type )
-      array << Word.find_or_create_by( word: word, type: type_object )
-    end
+    # @user_vocab = UserVocab.new( user_words: @user_words )
+    # @user_vocab.user_id = session[:user_id]
+    # @user_vocab.madlib_id = params[user_vocab.madlib.id]
+    # if @user_vocab.save
+    #   flash[:user_vocab] = @user_vocab.id
+    #   redirect_to madlib_path( @user_vocab.madlib_id )
+    # else
+    #   redirect_to "/madlibs/#{flash[:madlib_id]}/user_vocabs/new"
+    #   flash.keep
+    # end
+  end
 
-    @user = User.find( session[:user_id] )
-    @user_words = @words.map do |word|
-      UserWord.create( user_id: @user.id, word_id: word.id)
-    end
-    @user_vocab = UserVocab.new( user_words: @user_words )
-    @user_vocab.user_id = session[:user_id]
-    @user_vocab.madlib_id = flash[:madlib_id]
-    if @user_vocab.save
-      flash[:user_vocab] = @user_vocab.id
-      redirect_to madlib_path( @user_vocab.madlib_id )
-    else
-      redirect_to "/madlibs/#{flash[:madlib_id]}/user_vocabs/new"
-      flash.keep
-    end
+  def show
+    @madlib = Madlib.find(params[:madlib_id])
+    @user_vocab = UserVocab.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: :destroy
   resources :madlibs, except: :destroy do
-    resources :user_vocabs, only: [:new, :create]
+    resources :user_vocabs, only: [:new, :create, :show]
   end
   resources :words, only: [:index, :new, :create]
   resources :user_vocabs, except: [:new, :destroy]
